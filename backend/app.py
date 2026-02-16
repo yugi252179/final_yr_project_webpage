9ef5cc4dec6c69302aae3700a0b03081a90f91ef
yugi252179 <ye252179@gmail.com>
2026-02-16 14:27:25 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -624,8 +624,3 @@ def brain():
         print("❌ Error:", e)
         return jsonify({'error': 'Error processing image'}), 500
 
-if __name__ == '__main__':
-    print("Starting Enhanced Cardiovascular Risk  and Glaucoma Assessment API (V9 - Final Viz Fix) on Port 5000...")
-    # IMPORTANT: Use use_reloader=False if you see caching issues after repeated saves.
-    app.run(debug=True, port=5000)
-
